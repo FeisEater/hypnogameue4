93841ec3aa5029ac48f81f17019d0be003858261
Pavel Smirnov <pavelsmirnov94@gmail.com>
2015-04-14 02:36:41 +0300

    cant cast dynamically

--- Source/HypnoToad/HSound.cpp
+++ Source/HypnoToad/HSound.cpp
@@ -3,3 +3,15 @@
 #include "HypnoToad.h"
 #include "HSound.h"
 
+bool HWord::Compare(const HSound& rhs)
+{
+	const HWord* pRhsWord = dynamic_cast<const HWord*>(&rhs);
+	if (!pRhsWord)	return false;
+	return content == pRhsWord->content;
+}
+
+bool HGunShot::Compare(const HSound& rhs)
+{
+	const HGunShot* pRhsShot = dynamic_cast<const HGunShot*>(&rhs);
+	return pRhsShot ? true : false;
+}
--- Source/HypnoToad/HSound.h
+++ Source/HypnoToad/HSound.h
@@ -10,11 +10,19 @@ class HYPNOTOAD_API HSound
 public:
 	HSound(FVector origin, float range) : m_origin(origin), m_range(range) {}
 
-	static void BroadCastSound(HSound sound);
+	static void BroadCastSound(HSound& sound);
+
+	bool operator==(const HSound& rhs)
+	{
+		return Compare(rhs);
+	}
 
 protected:
 	FVector m_origin;
 	float m_range;
+
+private:
+	virtual bool Compare(const HSound& rhs) = 0;
 };
 
 class HWord : public HSound
@@ -22,10 +30,16 @@ class HWord : public HSound
 public:
 	HWord(FVector origin, FString con) : HSound(origin, 700), content(con) {}
 	FString content;
+
+private:
+	virtual bool Compare(const HSound& rhs);
 };
 
 class HGunShot : public HSound
 {
 public:
 	HGunShot(FVector origin) : HSound(origin, 5000) {}
+
+private:
+	virtual bool Compare(const HSound& rhs);
 };
